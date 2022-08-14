9bcfd2e1e3a8a0923ea9570b620e5260ee9f8847
Mohammad Safia <mohammad.safia@meccasoft.com>
2022-08-15 02:00:22 +0300

    complete about us section

--- frontend-react/src/container/About/About.js
+++ frontend-react/src/container/About/About.js
@@ -1,10 +1,37 @@
 import './About.scss';
 
 import React from 'react';
+import {motion} from "framer-motion";
+import {images} from "../../constants";
 
+const abouts = [
+    {title: 'Web Development', description: 'I am a good web developer', imgUrl: images.about01},
+    {title: 'Frontend Development', description: 'I am a good web developer', imgUrl: images.about02},
+    {title: 'Backend Development', description: 'I am a good web developer', imgUrl: images.about03},
+    {title: 'MERN Stack', description: 'I am a good web developer', imgUrl: images.about04},
+];
 const About = () => {
     return (
-        <div> About is working!</div>
+        <>
+            <h2 className="head-text">
+                I Know That <span>Good Development</span> <br/> means <span>Good Business</span>
+            </h2>
+            <div className="app__profiles">
+                {abouts.map((about, index) => (
+                    <motion.div
+                        whileInView={{opacity: 1,}}
+                        whileHover={{scale: 1.1}}
+                        transition={{duration: 0.5, type: 'tween'}}
+                        className="app__profiles-item"
+                        key={about.title + index}
+                    >
+                        <img src={about.imgUrl} alt={about.title}/>
+                        <h2 className="bold-text" style={{marginTop: 20}}>{about.title}</h2>
+                        <p className="p-text" style={{marginTop: 10}}>{about.description}</p>
+                    </motion.div>
+                ))}
+            </div>
+        </>
     );
 };
 
