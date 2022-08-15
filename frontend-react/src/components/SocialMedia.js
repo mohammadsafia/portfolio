567d58de39ae6233e660f40fa10cf7f983085596
Mohammad Safia <mohammad.safia@meccasoft.com>
2022-08-15 16:38:46 +0300

    update social to be links

--- frontend-react/src/components/SocialMedia.js
+++ frontend-react/src/components/SocialMedia.js
@@ -1,19 +1,19 @@
 import React from 'react';
-import {BsInstagram, BsTwitter} from "react-icons/bs";
+import {BsInstagram, BsLinkedin, BsWhatsapp} from "react-icons/bs";
 import {FaFacebookF} from 'react-icons/fa'
 
 const SocialMedia = () => {
     return (
         <div className="app__social">
-            <div>
-                <BsTwitter/>
-            </div>
-            <div>
+            <a href="https://wa.me/+962780812426?lang=en" target="_blank">
+                <BsWhatsapp/>
+            </a>
+            <a href="https://www.facebook.com/mohamad.safia.3/" target="_blank">
                 <FaFacebookF/>
-            </div>
-            <div>
-                <BsInstagram/>
-            </div>
+            </a>
+            <a href="https://www.linkedin.com/in/mohammad-safia/" target="_blank">
+                <BsLinkedin/>
+            </a>
         </div>
     );
 };
