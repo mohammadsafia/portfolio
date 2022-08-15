64ad8e8dbd9504e2ded205cfc6ea88b31a8c9aab
Mohammad Safia <mohammad.safia@meccasoft.com>
2022-08-15 16:39:04 +0300

    remove unused imports

--- frontend-react/src/components/SocialMedia.js
+++ frontend-react/src/components/SocialMedia.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {BsInstagram, BsLinkedin, BsWhatsapp} from "react-icons/bs";
+import {BsLinkedin, BsWhatsapp} from "react-icons/bs";
 import {FaFacebookF} from 'react-icons/fa'
 
 const SocialMedia = () => {
